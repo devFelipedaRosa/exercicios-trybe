09f41eae0e787a39a67f5613cac46f9836f138a2
Felipe da Rosa <felipedarosa@live.com>
2023-08-17 17:52:34 -0300

    exercicio2 pos-aula if-else

---
+++ fundamentos/secao-3/dia-1/script4.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 10;
+let maiornumero;
+if (num1 > num2){
+    maiornumero = num1;
+}else if (num1 < num2){
+    maiornumero = num2;
+}else{
+    maiornumero = 'iguais';
+}
+console.log(maiornumero);

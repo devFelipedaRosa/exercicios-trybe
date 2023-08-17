ff5f1dab0b82fc286d3a9c9dc218577d264e9fbd
Felipe da Rosa <felipedarosa@live.com>
2023-08-17 20:08:15 -0300

    atualiza script5

--- fundamentos/secao-3/dia-1/script5.js
+++ fundamentos/secao-3/dia-1/script5.js
@@ -1,6 +1,6 @@
-let num1 = 50;
-let num2 = 50;
-let num3 = 30;
+let num1 = 80;
+let num2 = 80;
+let num3 = 80;
 let maiornumero;
 if (num1 > num2 && num1 > num3){
     maiornumero = num1;
@@ -8,7 +8,13 @@ if (num1 > num2 && num1 > num3){
     maiornumero = num2;
 }else if (num3 > num1 && num3 > num2){
     maiornumero = num3;
-}else{
-    maiornumero = 'iguais';
+}else if (num1 === num2){
+    maiornumero = 'numero 1 e numero 2 iguais';
+}else if (num1 === num3){
+    maiornumero = 'numero 1 e numero 3 iguais';
+}else if (num2 === num3){
+    maiornumero = 'numero 2 e numero 3 iguais';
+}else if (num1 === num2 && num1 === num3){
+    maiornumero = 'numeros iguais'
 }
 console.log(maiornumero);

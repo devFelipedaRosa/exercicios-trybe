0bfb99dea24a30690248622c933fdd40259da0c2
Felipe da Rosa <felipedarosa@live.com>
2023-08-20 11:38:05 -0300

    pra fixar exrcicio 2

---
+++ fundamentos/secao-3/dia-3/script2.js
@@ -0,0 +1,19 @@
+const trybeBankCustomers = ['Oliva', 'Nat', 'Gus'];
+
+function addCustomer(newCustomers) {
+    for (let index = 0; index < newCustomers.length; index+=1) {
+        if (typeof newCustomers[index] === 'string') {
+            trybeBankCustomers.push(newCustomers[index]);
+            return 'Clientes adiconados com sucesso';
+        } else {
+          return 'Todos os valores precisam ser do tipo string';
+        }
+        
+    }
+    return trybeBankCustomers;
+  
+  
+}
+console.log(addCustomer(['Felipe','Maria']));
+console.log(addCustomer([true,25,'Felipe']));
+console.log(trybeBankCustomers);
